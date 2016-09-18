b5da5841a3dd522c2021337532a9de72027705e0
David Crowther <crowtherdr@familysearch.org>
2016-09-18 03:52:53 -0600

    Adds content.

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+var expressLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.set('layout', 'layout') // defaults to 'layout'
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/assets'));
+app.use(expressLayouts);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(bodyParser());
+
+app.get('/', function(request, response){
+  response.render("index", { layout:'layout.ejs' });
+});
+app.get('/html-css/examples', function(request, response){
+  response.render("html-css/examples", { layout:'layout.ejs' });
 });
 
 app.listen(app.get('port'), function() {
