89b097c1165b88cd5184a5f5f616ade53cf86a4b
David Crowther <crowtherdr@familysearch.org>
2016-10-21 18:36:40 -0600

    Adds CSS animation example.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,2 +1,9 @@
+var animateMe = function() {
+  $(".css-animations p.button-wrapper button").addClass("animate-me");
+  window.setTimeout(function() {
+    $(".css-animations p.button-wrapper button").removeClass("animate-me");
+  }, 4000)
+}
+
 $(document).ready(function () {
 });
