ac9804492748b260531454e1bfcf2119e3297163
David Crowther <crowtherdr@familysearch.org>
2016-10-26 01:19:27 -0600

    Adds the "revising" CSS examples.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -5,5 +5,24 @@ var animateMe = function() {
   }, 4000)
 }
 
+var scrollContainer = function(container) {
+  $(container).scrollTop(200);
+}
+
+var changeToOverflow = function(container) {
+  $(container).css("overflow", "hidden");
+}
+
+var toggleBoxDisplay = function(container) {
+  var container = $(container);
+
+  if (!container.css("box-supress") || container.css("box-supress") === "show") {
+    container.css("overflow", "hide");
+  }
+  else {
+    container.css("overflow", "show");
+  }
+}
+
 $(document).ready(function () {
 });
