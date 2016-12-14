84c8fc4809acea259c45f2d280cc36833a3cf212
David Crowther <crowtherdr@familysearch.org>
2016-12-14 15:39:11 -0700

    Switches POJ web component implementation.
    Adds JavaScript call to append <image-vignette>

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -30,62 +30,15 @@ var animateFish = function() {
   fish.animate(positionKeyframes, positionTiming);
 }
 
-// class ImageVignette extends HTMLElement {
-//   constructor() {
-//     super();
-//     this._imagePath = null;
-//   }
-
-//   static get observedAttributes() { return ["height", "src", "width"]; }
-
-//   attributeChangedCallback(wcAttributeName, oldValue, newValue) {
-// console.log('wcAttributeName', wcAttributeName);
-//     // switch (wcAttributeName) {
-//     //   case "height":
-//     //     this._height = newValue;
-//     //     break;
-
-//     //   case "src":
-//     //     this._imagePath = newValue;
-//     //     break;
-
-//     //   case "width":
-//     //     this._width = newValue;
-//     //     break;
-//     // }
-
-//     this._updateRendering(wcAttributeName, newValue);
-//   }
-//   connectedCallback() {
-// console.log("LAST");
-//     this._whenDoesThisFire();
-//   }
-
-//   get src() {
-//     return this._imagePath;
-//   }
-//   set src(v) {
-//     this.setAttribute("src", v);
-//   }
-
-//   _whenDoesThisFire() {
-//   }
-
-//   _updateRendering(attributeName, attributeValue) {
-// console.log("FIRST");
-//     if (this.ownerDocument && this.ownerDocument.defaultView && !this.hasChildNodes()) {
-//       this.imgNode = document.createElement("img");
-//       this.appendChild(this.imgNode);
-//     }
-
-//     this.childNodes[0][attributeName] = attributeValue;
-//   }
-// }
-
-// customElements.define("image-vignette", ImageVignette);
-
-
+// https://w3c.github.io/webcomponents/spec/custom/
 class ImageVignette extends HTMLElement {
+  constructor() {
+    super();
+    this._imagePath = null;
+  }
+
+  static get observedAttributes() { return ["height", "src", "width"]; }
+
   addCSS() {
     var cssId = 'ImageVignette';  // you could encode the css path itself to generate id..
     if (!document.getElementById(cssId))
@@ -101,51 +54,117 @@ class ImageVignette extends HTMLElement {
     }
   }
 
-  constructor() {
-    // Always call super first in constructor
-    super();
+  attributeChangedCallback(wcAttributeName, oldValue, newValue) {
+// console.log('wcAttributeName', wcAttributeName);
+    // switch (wcAttributeName) {
+    //   case "height":
+    //     this._height = newValue;
+    //     break;
 
-    // Create a shadow root
-    var shadow = this.attachShadow({mode: 'open'});
+    //   case "src":
+    //     this._imagePath = newValue;
+    //     break;
 
-    shadow.innerHTML = '<style>img { display: block;}</style>';
+    //   case "width":
+    //     this._width = newValue;
+    //     break;
+    // }
 
-
-    var div = document.createElement('div');
-    div.className = 'fade';
-    // Add the div to the shadow root.
-    shadow.appendChild(div);
-
-    // Create a standard img element and set it's attributes.
-    var img = document.createElement('img');
-    img.alt = this.getAttribute('alt') || "";
-    img.height = this.getAttribute('height') || "";
-    img.src = this.getAttribute('src');
-    img.width = this.getAttribute('width') || "";
-    // img.className = 'product-img';
-
-    // Add the image to the shadow root.
-    shadow.appendChild(img);
+    this._updateRendering(wcAttributeName, newValue);
+  }
+  connectedCallback() {
+console.log("LAST"); // Trying to figure out what this event means.
     this.addCSS();
+    this._whenDoesThisFire();
+  }
 
-    // Add an event listener to the image.
-    // img.addEventListener('click', () => {
-    //   window.location = this.getAttribute('data-url');
-    // });
+  get src() {
+    return this._imagePath;
+  }
+  set src(v) {
+    this.setAttribute("src", v);
+  }
 
-    // Create a link to the product.
-    // var link = document.createElement('a');
-    // link.innerText = this.getAttribute('data-name');
-    // link.href = this.getAttribute('data-url');
-    // link.className = 'product-name';
+  _whenDoesThisFire() {
+  }
 
-    // Add the link to the shadow root.
-    // shadow.appendChild(link);
+  _updateRendering(attributeName, attributeValue) {
+console.log("FIRST");
+    if (this.ownerDocument && this.ownerDocument.defaultView && !this.hasChildNodes()) {
+      this.imgNode = document.createElement("img");
+      this.appendChild(this.imgNode);
+    }
+
+    this.childNodes[0][attributeName] = attributeValue;
   }
 }
 
-// Define the new element
-customElements.define('image-vignette', ImageVignette);
+customElements.define("image-vignette", ImageVignette);
+
+
+// https://developer.mozilla.org/en-US/docs/Web/Web_Components/Custom_Elements
+// This seems easier to understand, but does not have the callbacks.
+// class ImageVignette extends HTMLElement {
+//   addCSS() {
+//     var cssId = 'ImageVignette';  // you could encode the css path itself to generate id..
+//     if (!document.getElementById(cssId))
+//     {
+//         var head  = document.getElementsByTagName('head')[0];
+//         var link  = document.createElement('link');
+//         link.id   = cssId;
+//         link.rel  = 'stylesheet';
+//         link.type = 'text/css';
+//         link.href = '/css/image-vignette.css';
+//         link.media = 'all';
+//         head.appendChild(link);
+//     }
+//   }
+
+//   constructor() {
+//     // Always call super first in constructor
+//     super();
+
+//     // Create a shadow root
+//     var shadow = this.attachShadow({mode: 'open'});
+
+//     shadow.innerHTML = '<style>img { display: block;}</style>';
+
+
+//     var div = document.createElement('div');
+//     div.className = 'fade';
+//     // Add the div to the shadow root.
+//     shadow.appendChild(div);
+
+//     // Create a standard img element and set it's attributes.
+//     var img = document.createElement('img');
+//     img.alt = this.getAttribute('alt') || "";
+//     img.height = this.getAttribute('height') || "";
+//     img.src = this.getAttribute('src');
+//     img.width = this.getAttribute('width') || "";
+//     // img.className = 'product-img';
+
+//     // Add the image to the shadow root.
+//     shadow.appendChild(img);
+//     this.addCSS();
+
+//     // Add an event listener to the image.
+//     // img.addEventListener('click', () => {
+//     //   window.location = this.getAttribute('data-url');
+//     // });
+
+//     // Create a link to the product.
+//     // var link = document.createElement('a');
+//     // link.innerText = this.getAttribute('data-name');
+//     // link.href = this.getAttribute('data-url');
+//     // link.className = 'product-name';
+
+//     // Add the link to the shadow root.
+//     // shadow.appendChild(link);
+//   }
+// }
+
+// // Define the new element
+// customElements.define('image-vignette', ImageVignette);
 
 
 
@@ -163,4 +182,13 @@ customElements.define('image-vignette', ImageVignette);
 
 
 $(document).ready(function () {
+  var javascriptImageVignette = document.getElementById("JavascriptImageVignette") || null;
+
+  if (javascriptImageVignette) {
+    const imageVignette = document.createElement("image-vignette");
+    imageVignette.setAttribute('height', 100);
+    imageVignette.setAttribute('src', '/img/casual-me.jpg');
+    imageVignette.setAttribute('width', 100);
+    javascriptImageVignette.appendChild(imageVignette);
+  }
 });
