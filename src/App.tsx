4e1835dda44d0104094c325ee8b40b550d6481c2
David Crowther <crowtherdr@familysearch.org>
2021-09-18 01:40:23 -0600

    Updates header font & colors

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,11 @@ function App(): JSX.Element {
         <header className="Hero-section">
           <div>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-              <h1 className="hero-title">Hi, I&apos;m David Crowther. I&apos;m a web developer.</h1>
+              <h1 className="hero-title">
+                Hi, my name is {''}
+                <span className="text-color-main">David Crowther</span>.<br /> I&apos;m a web
+                developer.
+              </h1>
             </Fade>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <p className="hero-cta">
