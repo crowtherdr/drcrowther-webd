96bb6df0a7d48b17b22ab9424f3cb1f978b6937d
David Crowther <crowtherdr@familysearch.org>
2021-10-15 16:31:01 -0600

    Belongs to last commit

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/who am i/i)
-  expect(linkElement).toBeInTheDocument()
-})
