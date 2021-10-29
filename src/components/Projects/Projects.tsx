6e9810ee8d5282f63ecc89dbd671063c936522fe
David Crowther <crowtherdr@familysearch.org>
2021-10-29 15:53:16 -0600

    Adds Marketing project

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,14 +2,30 @@ import React, { useContext, useEffect, useState } from 'react'
 import Fade from 'react-reveal/Fade'
 import Tilt from 'react-tilt'
 import ProjectsContent from '../../content/projects_content_en.json'
-import laHomelandProjectExample from '../../assets/la-homeland-dia-de-los-muertos.jpg'
-import familyTreeProjectExample from '../../assets/family-tree-overview-preview.jpg'
+import laHomelandProjectExample from '../../assets/project-examples/la-homeland-dia-de-los-muertos.jpg'
+import familyTreeProjectExample from '../../assets/project-examples/family-tree-overview.jpg'
+import aboutFS from '../../assets/project-examples/about-fs.jpg'
+import conferenceInBox from '../../assets/project-examples/conference-in-a-box.jpg'
+import donateToday from '../../assets/project-examples/donate-today.jpg'
+import fsArchives from '../../assets/project-examples/fs-archives.jpg'
+import fsTermsOfUse from '../../assets/project-examples/fs-terms-of-use.jpg'
+import myFamily from '../../assets/project-examples/my-family.jpg'
+import personalAncestralFile from '../../assets/project-examples/personal-ancestral-file.jpg'
+import recordsPresMissionOpps from '../../assets/project-examples/records-preservation-mission-opportunities.jpg'
+import volunteer from '../../assets/project-examples/volunteer.jpg'
+import whatsNew from '../../assets/project-examples/whats-new.jpg'
 
 const exampleImages = ['', laHomelandProjectExample, familyTreeProjectExample, '', '', '']
+const exampleMarketingPages = [aboutFS, fsArchives, donateToday, conferenceInBox, myFamily, personalAncestralFile, recordsPresMissionOpps, fsTermsOfUse, volunteer, whatsNew]
 
 const Projects = () => {
   const [isDesktop, setIsDesktop] = useState(false)
   const [isMobile, setIsMobile] = useState(false)
+  const [whichMarketingExample, setWhichMarketingExample] = useState(0)
+
+  // const showMarketingImage = (evt, exampleImage) => {
+  //   document.getElementById('marketingExample').src = exampleImage
+  // }
 
   useEffect(() => {
     if (window.innerWidth > 769) {
@@ -31,8 +47,8 @@ const Projects = () => {
           <h2 className="section-title">{ProjectsContent?.pageContent?.mainHeading}</h2>
           {ProjectsContent?.pageContent?.projects.map(
                       (project, index) => {
-            const { whichProject, title, description, projectDemoUrl = [], projectDemoUrl2 = [] } = project;
-            
+            const { whichProject, title, description, projectDemoUrl = [], projectDemoUrl2 = [], examples = [] } = project;
+            // const exampleLinks = examples[0]?.links || []
 
             return (
               <div key={index} className="project-wrapper row">
@@ -54,7 +70,7 @@ const Projects = () => {
                           </p>
                           <p className="mb-4">{false || ''}</p>
                         </div>
-                        {projectDemoUrl.length && (<a
+                        {!!projectDemoUrl.length && (<a
                           target="_blank"
                           rel="noopener noreferrer"
                           className="cta-btn cta-btn--hero"
@@ -71,7 +87,27 @@ const Projects = () => {
                         >
                           {projectDemoUrl2[0] ? projectDemoUrl2[0] : ("See Live")}
                         </a>)}
-
+                        {!!examples.length && (
+                          <>
+                            <p>{examples[0].heading}</p>
+                              <ol>
+                              {!!examples.length && examples[0]?.links && examples[0]?.links.map((exampleLink, index) => {
+                                const elementKey = `key${index}`
+                                return (<li key={elementKey}><a
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  className="cta-btn"
+                                  href={exampleLink.url}
+                                  onMouseEnter={() => {
+                                    setWhichMarketingExample(index)
+                                  }}
+                                >
+                                  {exampleLink.text}
+                                </a></li>)
+                              })}
+                              </ol>
+                          </>
+                        )}
                         {false && (
                           <a
                             target="_blank"
@@ -95,7 +131,7 @@ const Projects = () => {
                     >
                       <div className="project-wrapper__image">
                         <a
-                          href={projectDemoUrl[1]}
+                          href={whichProject !== 3 ? projectDemoUrl[1]: examples[0]?.links[whichMarketingExample]?.url}
                           target="_blank"
                           aria-label="Project Link"
                           rel="noopener noreferrer"
@@ -114,8 +150,9 @@ const Projects = () => {
                             }}
                           >
                             <div data-tilt className="thumbnail rounded">
-                              { exampleImages[whichProject] && (<img alt={'alt'} src={exampleImages[whichProject]} />)}
+                              { whichProject !== 3 && exampleImages[whichProject] && (<img alt={'alt'} src={exampleImages[whichProject]} />)}
                               {/* <ProjectImg alt={'title'} filename={'img'} /> */}
+                              { whichProject === 3 && exampleMarketingPages[0] && (<img id="marketingExample" alt={'alt'} src={exampleMarketingPages[whichMarketingExample]} />)}
                             </div>
                           </Tilt>
                         </a>
