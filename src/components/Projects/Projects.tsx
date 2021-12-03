265f74e17294df3bd42c92bcd91a486b5e701b72
David Crowther <crowtherdr@familysearch.org>
2021-12-02 21:17:34 -0700

    Makes projectWrapper css

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -30,33 +30,6 @@ const projectsCss = css`
   margin-top: -15rem;
   padding-top: 15rem;
 
-  .project-wrapper {
-    margin-bottom: 15rem;
-
-    .row {
-      margin-bottom: 8rem;
-    }
-
-    &__text {
-      text-align: left;
-
-      &-title {
-        font-weight: bold;
-        margin-bottom: 1.8rem;
-        font-size: 2.5rem;
-      }
-
-      & p > a {
-        color: #4589ff;
-      }
-    }
-
-    &__image {
-      width: 90%;
-      margin: 0 auto;
-    }
-  }
-
   @media (max-width: 990px) {
     padding-right: 2rem;
     padding-left: 2rem;
@@ -67,9 +40,36 @@ const projectsCss = css`
   }
 
   @media (max-width: 1200px) {
-    .project-wrapper__text a + a {
+    .text a + a {
       margin-top: 12px;
     }
+  }
+`
+
+const projectWrapper = css`
+  margin-bottom: 15rem;
+
+  .row {
+    margin-bottom: 8rem;
+  }
+
+  .text {
+    text-align: left;
+
+    .title {
+      font-weight: bold;
+      margin-bottom: 1.8rem;
+      font-size: 2.5rem;
+    }
+
+    & p > a {
+      color: #4589ff;
+    }
+  }
+
+  .image {
+    width: 90%;
+    margin: 0 auto;
   }
 `
 
@@ -116,8 +116,7 @@ const Projects = () => {
   return (
     <section className={projectsCss}>
       <div className="container">
-        <div className="project-wrapper">
-          {/* <Title title="Projects" /> */}
+        <div className={projectWrapper}>
           <h2 className="section-title">{ProjectsContent?.pageContent?.mainHeading}</h2>
           {ProjectsContent?.pageContent?.projects.map(
                       (project, index) => {
@@ -125,105 +124,103 @@ const Projects = () => {
             // const exampleLinks = examples[0]?.links || []
 
             return (
-              <div key={index} className="project-wrapper row">
-                <div className="row">
-                  <div className="col-lg-4 col-sm-12">
-                    <Fade
-                      left={isDesktop}
-                      bottom={isMobile}
-                      duration={1000}
-                      delay={500}
-                      distance="30px"
-                    >
-                      <div className="project-wrapper__text">
-                        <h3 className="project-wrapper__text-title">{title || 'Project Title'}</h3>
-                        <div>
-                          <p>
-                            {description ||
-                              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
-                          </p>
-                          <p className="mb-4">{false || ''}</p>
-                        </div>
-                        {!!projectDemoUrl.length && (<a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="cta-btn cta-btn--hero"
-                          href={projectDemoUrl[1]}
-                        >
-                          {projectDemoUrl[0] ? projectDemoUrl[0] : ("See Live")}
-                        </a>)}
+              <div key={index} className="row">
+                <div className="col-lg-4 col-sm-12">
+                  <Fade
+                    left={isDesktop}
+                    bottom={isMobile}
+                    duration={1000}
+                    delay={500}
+                    distance="30px"
+                  >
+                    <div className="text">
+                      <h3 className="title">{title || 'Project Title'}</h3>
+                      <div>
+                        <p>
+                          {description ||
+                            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                        </p>
+                        <p className="mb-4">{false || ''}</p>
+                      </div>
+                      {!!projectDemoUrl.length && (<a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="cta-btn cta-btn--hero"
+                        href={projectDemoUrl[1]}
+                      >
+                        {projectDemoUrl[0] ? projectDemoUrl[0] : ("See Live")}
+                      </a>)}
 
-                        {!!examples.length && (
-                          <>
-                            <p>{examples[0].heading}</p>
-                              <ol>
-                              {!!examples.length && examples[0]?.links && examples[0]?.links.map((exampleLink, index) => {
-                                const elementKey = `key${index}`
-                                return (<li key={elementKey}><a
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  className="cta-btn"
-                                  href={exampleLink.url}
-                                  onMouseEnter={() => {
-                                    setWhichMarketingExample(index)
-                                  }}
-                                >
-                                  {exampleLink.text}
-                                </a></li>)
-                              })}
-                              </ol>
-                          </>
-                        )}
-                        {false && (
-                          <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="cta-btn text-color-main"
-                            href={'repo'}
-                          >
-                            Source Code
-                          </a>
-                        )}
-                      </div>
-                    </Fade>
-                  </div>
-                  <div className="col-lg-8 col-sm-12">
-                    <Fade
-                      right={isDesktop}
-                      bottom={isMobile}
-                      duration={1000}
-                      delay={1000}
-                      distance="30px"
-                    >
-                      <div className="project-wrapper__image">
+                      {!!examples.length && (
+                        <>
+                          <p>{examples[0].heading}</p>
+                            <ol>
+                            {!!examples.length && examples[0]?.links && examples[0]?.links.map((exampleLink, index) => {
+                              const elementKey = `key${index}`
+                              return (<li key={elementKey}><a
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="cta-btn"
+                                href={exampleLink.url}
+                                onMouseEnter={() => {
+                                  setWhichMarketingExample(index)
+                                }}
+                              >
+                                {exampleLink.text}
+                              </a></li>)
+                            })}
+                            </ol>
+                        </>
+                      )}
+                      {false && (
                         <a
-                          href={whichProject !== 3 ? projectDemoUrl[1]: examples[0]?.links[whichMarketingExample]?.url}
                           target="_blank"
-                          aria-label="Project Link"
                           rel="noopener noreferrer"
+                          className="cta-btn text-color-main"
+                          href={'repo'}
                         >
-                          <Tilt
-                            options={{
-                              reverse: false,
-                              max: 8,
-                              perspective: 1000,
-                              scale: 1,
-                              speed: 300,
-                              transition: true,
-                              axis: null,
-                              reset: true,
-                              easing: 'cubic-bezier(.03,.98,.52,.99)',
-                            }}
-                          >
-                            <RoundedDiv data-tilt className={projectThumbnail}>
-                              { whichProject !== 3 && exampleImages[whichProject] && (<img alt={'alt'} src={exampleImages[whichProject]} />)}
-                              { whichProject === 3 && exampleMarketingPages[0] && (<img id="marketingExample" alt={'alt'} src={exampleMarketingPages[whichMarketingExample]} />)}
-                            </RoundedDiv>
-                          </Tilt>
+                          Source Code
                         </a>
-                      </div>
-                    </Fade>
-                  </div>
+                      )}
+                    </div>
+                  </Fade>
+                </div>
+                <div className="col-lg-8 col-sm-12">
+                  <Fade
+                    right={isDesktop}
+                    bottom={isMobile}
+                    duration={1000}
+                    delay={1000}
+                    distance="30px"
+                  >
+                    <div className="image">
+                      <a
+                        href={whichProject !== 3 ? projectDemoUrl[1]: examples[0]?.links[whichMarketingExample]?.url}
+                        target="_blank"
+                        aria-label="Project Link"
+                        rel="noopener noreferrer"
+                      >
+                        <Tilt
+                          options={{
+                            reverse: false,
+                            max: 8,
+                            perspective: 1000,
+                            scale: 1,
+                            speed: 300,
+                            transition: true,
+                            axis: null,
+                            reset: true,
+                            easing: 'cubic-bezier(.03,.98,.52,.99)',
+                          }}
+                        >
+                          <RoundedDiv data-tilt className={projectThumbnail}>
+                            { whichProject !== 3 && exampleImages[whichProject] && (<img alt={'alt'} src={exampleImages[whichProject]} />)}
+                            { whichProject === 3 && exampleMarketingPages[0] && (<img id="marketingExample" alt={'alt'} src={exampleMarketingPages[whichMarketingExample]} />)}
+                          </RoundedDiv>
+                        </Tilt>
+                      </a>
+                    </div>
+                  </Fade>
                 </div>
               </div>
             )
