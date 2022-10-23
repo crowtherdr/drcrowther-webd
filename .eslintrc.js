7f7e17319054ee234addb5b0ca615505b6cf2514
David Crowther <crowtherdr@familysearch.org>
2022-10-22 20:17:41 -0600

    Updates dependencies
    - @testing-library/react
    - @testing-library/user-event

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',
   ],
   plugins: ['@typescript-eslint'],
